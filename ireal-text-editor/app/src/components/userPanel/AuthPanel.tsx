ab1f517436aa49d29ad5c037cc2a8346c4146b35
Maciej Zawiasa <zawiasam@gmail.com>
2018-01-22 14:46:20 +0100

    split panel into smaller parts

---
+++ ireal-text-editor/app/src/components/userPanel/AuthPanel.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as firebase from "firebase";
+import { AuthProfileMenu } from "./ProfileMenu/AuthProfileMenu";
+
+const panelElementStyle = { paddingRight: "8px" };
+
+interface AuthPanelProps {
+  photoURL: string | null;
+  displayName: string | null;
+}
+
+class AuthPanel extends React.Component<AuthPanelProps> {
+  constructor(props: AuthPanelProps) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={{ display: "flex" }}>
+        <div style={panelElementStyle}>
+          Welcome back
+          <br />
+          {this.props.displayName || ""}
+        </div>
+        <AuthProfileMenu photoUrl={this.props.photoURL} />
+      </div>
+    );
+  }
+}
+
+export { AuthPanel };
---
+++ ireal-text-editor/app/src/components/userPanel/ProfileMenu/AuthProfileMenu.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { RaisedButton } from "material-ui";
+
+interface ProfileMenuAuthProps {
+  photoUrl: string | null;
+}
+
+class AuthProfileMenu extends React.Component<ProfileMenuAuthProps> {
+  logoutHandle = () => {
+    firebase.auth().signOut();
+  };
+
+  render() {
+    return (
+      <div style={{ display: "flex" }}>
+        <div>
+          <img
+            style={{ height: "36px", width: "36px" }}
+            src={this.props.photoUrl || "/default/profile/avatar.png"}
+          />
+        </div>
+        <div>
+          <RaisedButton label="Logout" onClick={this.logoutHandle} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export { AuthProfileMenu };
