5fa85074d8f3f3d26eadda1229cc18aa1a39a5cb
Maciej Zawiasa <zawiasam@gmail.com>
2018-03-25 12:51:58 +0200

    list styling

--- ireal-text-editor/app/src/components/songList/SongList.tsx
+++ ireal-text-editor/app/src/components/songList/SongList.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { SongData } from "@ireal-text-editor/models";
+import List, { ListItem, ListItemText } from "material-ui/List";
 import { Link } from "react-router-dom";
+import { Redirect } from "react-router";
+import { Divider } from "material-ui";
 
 interface SongListProps {
   songs: SongData[];
@@ -8,7 +11,15 @@ interface SongListProps {
   getSongs: () => void;
 }
 
+interface SongListState {
+  navigate: string | null;
+}
+
 class SongList extends React.Component<SongListProps> {
+  state: SongListState = {
+    navigate: null
+  };
+
   componentDidMount() {
     this.props.getSongs();
   }
@@ -19,17 +30,40 @@ class SongList extends React.Component<SongListProps> {
     }
   }
 
+  navigeteTo = (url: string) => {
+    this.setState({ navigate: url });
+  };
+
   render() {
     const { songs } = this.props;
+    const { navigate } = this.state;
 
+    if (navigate) {
+      return <Redirect to={navigate} push={true} />;
+    }
+
+    const songsCount = songs.length;
     return (
       <div>
-        <ul>
+        <List component="nav">
           {songs &&
-            songs.map(song => {
-              return <li key={song.id}><Link to={`/editor/${song.id}`}>{song.title}</Link> </li>;
+            songs.map((song, index) => {
+              return (
+                <>
+                  <ListItem
+                    button
+                    key={song.id}
+                    onClick={() => {
+                      this.navigeteTo(`/editor/${song.id}`);
+                    }}
+                  >
+                    <ListItemText primary={song.title} />
+                  </ListItem>
+                  {index !== songsCount - 1 ? <Divider />: <></>}
+                </>
+              );
             })}
-        </ul>
+        </List>
       </div>
     );
   }
