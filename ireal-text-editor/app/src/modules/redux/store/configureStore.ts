6dd6d6835b5d3434314ae4b6f4e5de4afbfd44fb
Maciej Zawiasa <zawiasam@gmail.com>
2018-04-03 18:03:24 +0200

    wont work with production react

--- ireal-text-editor/app/src/modules/redux/store/configureStore.ts
+++ ireal-text-editor/app/src/modules/redux/store/configureStore.ts
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "@ireal-text-editor/redux-reducers";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
 
 function configureStore(initialState: any) {
